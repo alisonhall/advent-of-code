663f17178fdb17bbd5e0fa71993de9ae57ecde7a
Alison Hall <alison.hall@publicissapient.com>
2022-12-01 18:08:03 -0500

    Add final solution for Dec 1 2022

--- 2022/1/src/index.js
+++ 2022/1/src/index.js
@@ -1,17 +1,41 @@
 import data1 from "./data1.json";
 
 export const part1 = (data = data1) => {
-  return {
-    caloriesPerElf,
-    maxCalories
-  };
+    const caloriesPerElf = data.map((elfData, index) => {
+        return elfData.reduce((previousValue, currentValue, currentIndex, array) => {
+            if (index === 0) {
+                console.log({previousValue, currentValue});
+            }
+            return previousValue + Number(currentValue);
+        }, 0);
+    });
+
+    const maxCalories = Math.max(...caloriesPerElf);
+
+    return {
+        caloriesPerElf,
+        maxCalories
+    };
 };
 
 export const part2 = (data = data1) => {
-    return {};
+    const { caloriesPerElf } = part1(data);
+
+    function compareNumbers(a, b) {
+        return b - a;
+    }
+
+    caloriesPerElf.sort(compareNumbers);
+    const [top1, top2, top3] = caloriesPerElf;
+    const caloriesForTop3 = top1 + top2 + top3;
+
+    return {
+        caloriesPerElf,
+        caloriesForTop3
+    };
 };
 
 export default {
-  "Part 1": part1(data1)?.maxCalories,
-  "Part 2": part2(data1)
+    "Part 1": part1(data1)?.maxCalories,
+    "Part 2": part2(data1)?.caloriesForTop3
 };
--- 2022/1/src/index.test.js
+++ 2022/1/src/index.test.js
@@ -1,58 +1,72 @@
 import { part1, part2 } from "./index";
 
-describe("Tests for part1", () => {
-    describe("Given sample data matches given result", () => {
-        const sampleData = [
-            [
-                1000,
-                2000,
-                3000
-            ],
-            [
-                4000
-            ],
-            [
-                5000,
-                6000
-            ],
-            [
-                7000,
-                8000,
-                9000
-            ],
-            [
-                10000
-            ]
-        ];
+const sampleData = [
+    [
+        1000,
+        2000,
+        3000
+    ],
+    [
+        4000
+    ],
+    [
+        5000,
+        6000
+    ],
+    [
+        7000,
+        8000,
+        9000
+    ],
+    [
+        10000
+    ]
+];
 
-        const { maxCalories, caloriesPerElf } = part1(sampleData);
+describe("Tests for part1 - Given sample data matches given result", () => {
+    const { maxCalories, caloriesPerElf } = part1(sampleData);
 
-        test('First elf has a total of 6000 calories', () => {
-            expect(caloriesPerElf?.[0]).toEqual(6000);
-        });
+    test('First elf has a total of 6000 calories', () => {
+        expect(caloriesPerElf?.[0]).toEqual(6000);
+    });
 
-        test('Second elf has a total of 4000 calories', () => {
-            expect(caloriesPerElf?.[1]).toEqual(4000);
-        });
+    test('Second elf has a total of 4000 calories', () => {
+        expect(caloriesPerElf?.[1]).toEqual(4000);
+    });
 
-        test('Third elf has a total of 11000 calories', () => {
-            expect(caloriesPerElf?.[2]).toEqual(11000);
-        });
+    test('Third elf has a total of 11000 calories', () => {
+        expect(caloriesPerElf?.[2]).toEqual(11000);
+    });
 
-        test('Fourth elf has a total of 24000 calories', () => {
-            expect(caloriesPerElf?.[3]).toEqual(24000);
-        });
+    test('Fourth elf has a total of 24000 calories', () => {
+        expect(caloriesPerElf?.[3]).toEqual(24000);
+    });
 
-        test('Fifth elf has a total of 10000 calories', () => {
-            expect(caloriesPerElf?.[4]).toEqual(10000);
-        });
+    test('Fifth elf has a total of 10000 calories', () => {
+        expect(caloriesPerElf?.[4]).toEqual(10000);
+    });
 
-        test('Max calories is 24000', () => {
-            expect(maxCalories).toEqual(24000);
-        });
+    test('Max calories is 24000', () => {
+        expect(maxCalories).toEqual(24000);
     });
 });
 
-describe("Tests for part2", () => {
-    test.todo('To Do');
+describe("Tests for part2 - Given sample data matches given resul", () => {
+    const { caloriesPerElf, caloriesForTop3 } = part2(sampleData);
+
+    test('Elf with most calories', () => {
+        expect(caloriesPerElf?.[0]).toEqual(24000);
+    });
+
+    test('Elf with second most calories', () => {
+        expect(caloriesPerElf?.[1]).toEqual(11000);
+    });
+
+    test('Elf with third most calories', () => {
+        expect(caloriesPerElf?.[2]).toEqual(10000);
+    });
+
+    test('Calories for the top 3', () => {
+        expect(caloriesForTop3).toEqual(45000);
+    });
 });
