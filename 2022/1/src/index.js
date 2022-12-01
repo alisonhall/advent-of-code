c0c4b049240665bf674b5eb193d70ace160c33e1
Alison Hall <alison.hall@publicissapient.com>
2022-12-01 16:27:50 -0500

    Add starting point for Dec 1 2022 challenge

---
+++ 2022/1/src/index.js
@@ -0,0 +1,14 @@
+import data1 from "./data1.json";
+
+export const part1 = (data = data1) => {
+  return {};
+};
+
+export const part2 = (data = data1) => {
+    return {};
+};
+
+export default {
+  "Part 1": part1(data1),
+  "Part 2": part2(data1)
+};
---
+++ 2022/1/src/index.test.js
@@ -0,0 +1,11 @@
+import { part1, part2 } from "./index";
+
+const sampleData = null;
+
+describe("Tests for part1", () => {
+    test.todo('To Do');
+});
+
+describe("Tests for part2", () => {
+    test.todo('To Do');
+});
